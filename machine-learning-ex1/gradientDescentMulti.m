80e296abf62e23d1e32122273d8364b5e829b631
Actimermao <853830387@qq.com>
2018-02-05 15:51:37 +0800

    Add files via upload

---
+++ machine-learning-ex1/gradientDescentMulti.m
@@ -0,0 +1,45 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+temp = theta
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCostMulti) and gradient here.
+    %
+    for iter2 = 1:size(X,2)
+       temp(iter2) = theta(iter2) -  alpha/m*sum((X*theta-y).*X(:,iter2))
+    end
+    %x = theta(1) - alpha/m*sum((X*theta-y).*X(:,1))
+    %theta(2) = theta(2) - alpha/m*sum((X*theta-y).*X(:,2))
+    %theta(1) = x
+    theta = temp
+    J = computeCost(X, y, theta)
+    if iter ~= 1
+        %if J > J_history(iter-1)
+        %    break
+        %end
+        if abs(J-J_history(iter-1)) < 10^(-9)
+            break
+        end
+    end
+
+
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCostMulti(X, y, theta);
+
+end
+
+end
